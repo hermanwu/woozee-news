67e60ee8e9ecfe16ef63824ba0e141867711ba05
Herman Wu <zeesun.wu@gmail.com>
2021-04-03 21:10:52 -0700

    add top nav for news app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WoozeeLibModule } from 'woozee-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListSlidesComponent } from './helper/list-slides/list-slides.component';
 import { NewsPageComponent } from './news-page/news-page.component';
 import { NewsComponent } from './news/news.component';
 import { SharedAngularMaterialModule } from './shared/modules/shared-angular-material.module';
 import { ThoughtDisplayComponent } from './thought-display/thought-display.component';
-import { ListSlidesComponent } from './helper/list-slides/list-slides.component';
 
 @NgModule({
-  declarations: [AppComponent, NewsComponent, NewsPageComponent, ThoughtDisplayComponent, ListSlidesComponent],
+  declarations: [
+    AppComponent,
+    NewsComponent,
+    NewsPageComponent,
+    ThoughtDisplayComponent,
+    ListSlidesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedAngularMaterialModule,
+    WoozeeLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
